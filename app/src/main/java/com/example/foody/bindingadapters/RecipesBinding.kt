96bf058c193343d9a03c33b6e475a261c6c42eb1
Joseph Hu <josephhu706@hotmail.com>
2024-01-09 13:36:11 +1100

    setup bottomsheet and Dao

---
+++ app/src/main/java/com/example/foody/bindingadapters/RecipesBinding.kt
@@ -0,0 +1,50 @@
+package com.example.foody.bindingadapters
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.foody.data.database.RecipesEntity
+import com.example.foody.models.FoodRecipe
+import com.example.foody.util.NetworkResult
+
+class RecipesBinding {
+
+    companion object {
+        //binding for imageView
+        @BindingAdapter("readApiResponse", "readDatabase", requireAll = true)//we want our compiler to throw an error if we only specify one attribute
+        @JvmStatic
+        fun errorImageViewVisibility(
+            imageView: ImageView,
+            apiResponse: NetworkResult<FoodRecipe>?,
+            database: List<RecipesEntity>?
+        ){
+            if(apiResponse is NetworkResult.Error && database.isNullOrEmpty()){
+                imageView.visibility = View.VISIBLE
+                //if we have data in our database we want to show the data not the error
+            } else if (apiResponse is NetworkResult.Loading){
+                imageView.visibility = View.INVISIBLE
+            } else if (apiResponse is NetworkResult.Success) {
+                imageView.visibility = View.INVISIBLE
+            }
+        }
+        //binding for textView
+        @BindingAdapter("readApiResponse2", "readDatabase2", requireAll = true)
+        @JvmStatic
+        fun errorTextViewVisibility(
+            textView: TextView,
+            apiResponse: NetworkResult<FoodRecipe>?,
+            database: List<RecipesEntity>?
+        ){
+            if(apiResponse is NetworkResult.Error && database.isNullOrEmpty()){
+                textView.visibility = View.VISIBLE
+                textView.text = apiResponse.message.toString()
+                //if we have data in our database we want to show the data not the error
+            } else if (apiResponse is NetworkResult.Loading){
+                textView.visibility = View.INVISIBLE
+            } else if (apiResponse is NetworkResult.Success) {
+                textView.visibility = View.INVISIBLE
+            }
+        }
+    }
+}
