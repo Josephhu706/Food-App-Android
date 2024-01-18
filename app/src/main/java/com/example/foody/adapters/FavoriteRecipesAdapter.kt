632e531e8df9f5c5204284c0d0411567a7283771
Joseph Hu <josephhu706@hotmail.com>
2024-01-19 10:45:34 +1100

    Section 22 Contextual Action Mode complete

--- app/src/main/java/com/example/foody/adapters/FavoriteRecipesAdapter.kt
+++ app/src/main/java/com/example/foody/adapters/FavoriteRecipesAdapter.kt
@@ -1,15 +1,28 @@
 package com.example.foody.adapters
 
+import android.view.ActionMode
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.foody.R
 import com.example.foody.data.database.entities.FavoritesEntity
 import com.example.foody.databinding.FavoriteRecipesRowLayoutBinding
+import com.example.foody.ui.fragments.favorites.FavoriteRecipesFragmentDirections
 import com.example.foody.util.RecipesDiffUtil
+import kotlinx.android.synthetic.main.favorite_recipes_row_layout.view.*
 
-class FavoriteRecipesAdapter: RecyclerView.Adapter<FavoriteRecipesAdapter.MyViewHolder>() {
+class FavoriteRecipesAdapter(
+    private val requireActivity: FragmentActivity
+): RecyclerView.Adapter<FavoriteRecipesAdapter.MyViewHolder>(), ActionMode.Callback {
     private var favoriteRecipes = emptyList<FavoritesEntity>()
+    private var multiSelection = false
+    private var selectedRecipes = arrayListOf<FavoritesEntity>()
     class MyViewHolder(private val binding: FavoriteRecipesRowLayoutBinding): RecyclerView.ViewHolder(binding.root){
         fun bind(favoritesEntity: FavoritesEntity){
             binding.favoritesEntity = favoritesEntity
@@ -37,10 +50,65 @@ class FavoriteRecipesAdapter: RecyclerView.Adapter<FavoriteRecipesAdapter.MyView
         //dynamically get the position of each row and store it in selectedRecipe
         val selectedRecipe = favoriteRecipes[position]
         holder.bind(selectedRecipe)
+        //create onclick for each favorite recipe card to navigate to the details fragment of that card
+        holder.itemView.favoriteRecipesRowLayout.setOnClickListener{
+            val action =
+                FavoriteRecipesFragmentDirections.actionFavoriteRecipesFragmentToDetailsActivity(
+                    selectedRecipe.result
+                )
+            holder.itemView.findNavController().navigate(action)
+        }
+        //long lick listener
+        holder.itemView.favoriteRecipesRowLayout.setOnLongClickListener{
+            requireActivity.startActionMode(this)
+            true
+        }
+    }
+
+    private fun applySelection(holder: MyViewHolder, currentRecipe: FavoritesEntity){
+        if(selectedRecipes.contains(currentRecipe)){
+            selectedRecipes.remove(currentRecipe)
+        } else {
+            selectedRecipes.add(currentRecipe)
+        }
+    }
+
+    //this changes the stroke and background color of our favorite_recipes_row_layout when selected
+    private fun changeRecipeStyle(holder: MyViewHolder, backgroundColor: Int, strokeColor: Int){
+        holder.itemView.favoriteRecipesRowLayout.setBackgroundColor(
+            ContextCompat.getColor(requireActivity, backgroundColor)
+        )
+        holder.itemView.favorite_row_cardView.strokeColor =
+            ContextCompat.getColor(requireActivity, strokeColor)
     }
 
     override fun getItemCount(): Int {
         return favoriteRecipes.size
+    }
+
+    //inflate new menu
+    override fun onCreateActionMode(actionMode: ActionMode?, menu: Menu?): Boolean {
+        actionMode?.menuInflater?.inflate(R.menu.favorites_contextual_menu, menu)
+        applyStatusBarColor(R.color.contextualStatusBarColor)
+        return true
+    }
+
+    override fun onPrepareActionMode(p0: ActionMode?, p1: Menu?): Boolean {
+        return true
+    }
+
+    override fun onActionItemClicked(p0: ActionMode?, p1: MenuItem?): Boolean {
+        return true
+    }
+
+    override fun onDestroyActionMode(p0: ActionMode?) {
+        applyStatusBarColor(R.color.statusBarColor)
+    }
+
+    //change the color of the status bar color so that it matches the context menu action bar color
+    private fun applyStatusBarColor(color: Int){
+        requireActivity.window.statusBarColor =
+            ContextCompat.getColor(requireActivity, color)
     }
 
     fun setData(newFavoriteRecipes: List<FavoritesEntity>){
--- app/src/main/java/com/example/foody/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/foody/viewmodels/MainViewModel.kt
@@ -30,7 +30,6 @@ class MainViewModel @Inject constructor(
     /** ROOM DATABASE */
     val readRecipes: LiveData<List<RecipesEntity>> = repository.local.readDatabase().asLiveData()
     val readFavoriteRecipes: LiveData<List<FavoritesEntity>> = repository.local.readFavoriteRecipes().asLiveData()
-
     private fun insertRecipes(recipesEntity: RecipesEntity) = viewModelScope.launch(Dispatchers.IO) {
         repository.local.insertRecipes(recipesEntity)
     }
@@ -43,7 +42,7 @@ class MainViewModel @Inject constructor(
         repository.local.deleteFavoriteRecipes(favoritesEntity)
     }
 
-    private fun deleteAllFavoriteRecipes() =
+    fun deleteAllFavoriteRecipes() =
         viewModelScope.launch(Dispatchers.IO) {
             repository.local.deleteAllFavoriteRecipes()
         }
