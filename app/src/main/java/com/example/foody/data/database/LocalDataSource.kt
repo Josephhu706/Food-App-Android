6dd9861d820a4fdc493c5c499b41d5960476401a
Joseph Hu <josephhu706@hotmail.com>
2024-01-22 13:07:15 +1100

    Complete half of the food joke fragment

--- app/src/main/java/com/example/foody/data/database/LocalDataSource.kt
+++ app/src/main/java/com/example/foody/data/database/LocalDataSource.kt
@@ -1,7 +1,9 @@
 package com.example.foody.data.database
 
 import com.example.foody.data.database.entities.FavoritesEntity
+import com.example.foody.data.database.entities.FoodJokeEntity
 import com.example.foody.data.database.entities.RecipesEntity
+import com.example.foody.models.FoodJoke
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
@@ -16,12 +18,20 @@ class LocalDataSource @Inject constructor(
         return recipesDao.readFavoriteRecipes()
     }
 
+    fun readFoodJoke(): Flow<List<FoodJoke>>{
+        return recipesDao.readFoodJoke()
+    }
+
     suspend fun insertRecipes(recipesEntity: RecipesEntity){
         recipesDao.insertRecipes(recipesEntity)
     }
 
     suspend fun insertFavoriteRecipes(favoritesEntity: FavoritesEntity) {
         recipesDao.insertFavoriteRecipe(favoritesEntity)
+    }
+
+    suspend fun insertFoodJoke(foodJokeEntity: FoodJokeEntity) {
+        recipesDao.insertFoodJoke(foodJokeEntity)
     }
 
     suspend fun deleteFavoriteRecipes(favoritesEntity: FavoritesEntity){
